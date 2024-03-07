8d3c45558943568de901559b0ffe3ec92a6d4ca9
FinnTVD <finnit.th@gmail.com>
2024-03-07 11:19:20 +0700

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -7,16 +7,16 @@ import twitter from "../public/imgs/twitter-icon.svg";
 import mess from "../public/imgs/mess-icon.svg";
 import facebook from "../public/imgs/facebook-icon.svg";
 import chevrons from "../public/imgs/chevrons.svg";
-import linkedin from '../public/imgs/linkedin.svg'
-import { ArticleJsonLd } from 'next-seo';
+import linkedin from "../public/imgs/linkedin.svg";
+import { ArticleJsonLd } from "next-seo";
 import backto from "../public/imgs/backto.svg";
-import { NextSeo } from 'next-seo';
+import { NextSeo } from "next-seo";
 
 import {
   EmailShareButton,
   FacebookShareButton,
   TwitterShareButton,
-  LinkedinShareButton
+  LinkedinShareButton,
 } from "react-share";
 
 import {
@@ -31,24 +31,18 @@ import {
 
 import { PostGrid } from "../components/layouts/Post Grid/PostGrid";
 import { getNews, show } from "../store/action/new";
-import {
-  getAllNews,
-  getOngttNews,
-  getRelatedNews,
-} from "../store/action/new";
+import { getAllNews, getOngttNews, getRelatedNews } from "../store/action/new";
 import Link from "next/link";
 
 export default function SingleNew(props) {
   const [newDetail, setNewDetail] = useState(props.NewDetail);
-  const [link, setLink] = useState('')
+  const [link, setLink] = useState("");
 
   var ROOT_URL = process.env.HOSTNAME_DEMO;
 
   useEffect(() => {
-    setLink(window.location.href)
-  })
-
-
+    setLink(window.location.href);
+  });
 
   const convertDate = (date) => {
     if (date) {
@@ -59,17 +53,17 @@ export default function SingleNew(props) {
     const day = newDate.getDate();
     const month = newDate.toLocaleString("en-us", { month: "long" });
     const year = newDate.getFullYear();
-    return day + " " + month + ", " + year
+    return day + " " + month + ", " + year;
   };
 
   const router = useRouter();
 
   return (
     <>
-      {props.NewDetail &&
+      {props.NewDetail && (
         <NextSeo
-          title={props.NewDetail ? props.NewDetail.title : 'TIM'}
-          description={props.NewDetail.slug || ''}
+          title={props.NewDetail ? props.NewDetail.title : "TIM"}
+          description={props.NewDetail.slug || ""}
           openGraph={{
             url: link,
             title: props.NewDetail.title,
@@ -79,33 +73,24 @@ export default function SingleNew(props) {
                 url: props.NewDetail.featuredImage.node.sourceUrl,
                 width: 800,
                 height: 600,
-
               },
-
             ],
-            siteName: 'TIM',
+            siteName: "TIM",
           }}
         />
-      }
-      {props.NewDetail &&
+      )}
+      {props.NewDetail && (
         <ArticleJsonLd
           url={link}
           title={props.NewDetail.title}
-          images={[
-            props.NewDetail.featuredImage.node.sourceUrl
-          ]}
+          images={[props.NewDetail.featuredImage.node.sourceUrl]}
           datePublished={props.NewDetail.date}
-
-
           description={props.NewDetail.slug}
           isAccessibleForFree={true}
         />
-      }
+      )}
 
       {props.NewDetail && (
-
-
-
         <div className="post-content-area">
           <div className="container">
             {/* <Link href="/news"> */}
@@ -147,16 +132,12 @@ export default function SingleNew(props) {
                     <Image src={twitter} alt="twitter_icon" />
                   </div>
                 </TwitterShareButton>
-                <LinkedinShareButton
-                  url={link}
-                >
+                <LinkedinShareButton url={link}>
                   <div className="icon">
                     <Image src={linkedin} alt="mess_icon" />
                   </div>
                 </LinkedinShareButton>
-                <FacebookShareButton
-                  url={link}
-                >
+                <FacebookShareButton url={link}>
                   <div className="icon">
                     <Image src={facebook} alt="facebook_icon" />
                   </div>
@@ -168,45 +149,36 @@ export default function SingleNew(props) {
       )}
 
       <PostGrid
-        heading={
-          <div>
-            related News & Insights
-          </div>
-        }
+        heading={<div>related News & Insights</div>}
         data={props.AllNew}
       />
-
     </>
   );
 }
 export async function getStaticPaths() {
   const res = await getOngttNews();
-  const data = await res;
-  const paths = data.data.data.posts.nodes.map((data) => ({
-    params: { slug: data.slug },
+  // const data = await res;
+  const paths = res?.data?.data?.posts?.nodes?.map((data) => ({
+    params: { slug: data?.slug },
   }));
-
-
 
   return {
     paths,
-    fallback: 'blocking',
+    fallback: "blocking",
   };
 }
 export async function getStaticProps({ params }) {
-
   const res = await show(params.slug);
-  const categories = res.data?.data.post.categories.nodes.map(
+  const categories = res?.data?.data?.post?.categories?.nodes?.map(
     (value) => value.slug
   );
 
   const [allNew4, res4] = await Promise.all([getRelatedNews(3, categories)]);
   return {
     props: {
-      NewDetail: res.data?.data.post || null,
+      NewDetail: res?.data?.data?.post || null,
       AllNew: allNew4?.data?.data?.posts?.nodes || null,
     },
-    revalidate: 1
-
+    revalidate: 1,
   };
 }
